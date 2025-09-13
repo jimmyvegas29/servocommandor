692e1d51455f6bdd71a815909a36c3b739e20dd3
jimmyvegas29 <jimmyvegas29@gmail.com>
2025-09-13 13:01:25 -0400

    new file:   Servo - Copy.kv 	modified:   Servo.kv 	modified:   main.py

--- main.py
+++ main.py
@@ -4,26 +4,38 @@ from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.modalview import ModalView
 from kivy.clock import Clock
 from mock_servo_communication import ServoCommunicator
+from kivy.core.window import Window
 
+Window.size = (800, 480)
 
 class NumberPadPopup(ModalView):
     def __init__(self, **kwargs):
         super(NumberPadPopup, self).__init__(**kwargs)
 
     def add_digit(self, digit):
+        if self.ids.numpad_display.text == 'Invalid Input':
+            self.ids.numpad_display.text = ''
         self.ids.numpad_display.text += digit
+        if int(self.ids.numpad_display.text) > 3000:
+            self.ids.numpad_display.color = (1,0,0,1)
 
     def clear_display(self):
         self.ids.numpad_display.text = ''
+        self.ids.numpad_display.color = (1,1,1,1)
 
     def delete_digit(self):
         self.ids.numpad_display.text = self.ids.numpad_display.text[:-1]
+        if self.ids.numpad_display.text != '' and int(self.ids.numpad_display.text) < 3000:
+            self.ids.numpad_display.color = (1,1,1,1)
 
     def submit_value(self):
         try:
             value = int(self.ids.numpad_display.text)
-            App.get_running_app().root.set_speed(value)
-            self.dismiss()
+            if int(self.ids.numpad_display.text) > 3000:
+                self.ids.numpad_display.color = (1,0,0,1)
+            else:
+                App.get_running_app().root.set_speed(value)
+                self.dismiss()
         except ValueError:
             self.ids.numpad_display.text = 'Invalid Input'
 
@@ -59,31 +71,30 @@ class ServoControl(BoxLayout):
     def toggle_direction(self, direction):
         self.direction = direction
         if self.direction == 'fwd':
-            self.ids.fwd_button.background_color = (0, 0.5, 1, 1)
-            self.ids.rev_button.background_color = (0.3, 0.3, 0.3, 1)
+            self.ids.fwd_button.state = 'down'
+            self.ids.rev_button.state = 'normal'
         else:
-            self.ids.fwd_button.background_color = (0.3, 0.3, 0.3, 1)
-            self.ids.rev_button.background_color = (0, 0.5, 1, 1)
+            self.ids.fwd_button.state = 'normal'
+            self.ids.rev_button.state = 'down'
         print(f"Toggle direction to: {self.direction}")
 
     def update_rpm_display(self):
         rpm_str = str(int(self.current_speed)).zfill(4)
-        print(self.ids)
         self.ids.rpm_digit_1.text = rpm_str[0]
         self.ids.rpm_digit_2.text = rpm_str[1]
         self.ids.rpm_digit_3.text = rpm_str[2]
         self.ids.rpm_digit_4.text = rpm_str[3]
 
         if self.current_speed < 1000:
-            self.ids.rpm_digit_1.color = (0.5, 0.5, 0.5, 1)
+            self.ids.rpm_digit_1.color = (0.13, 0.13, 0.13, 1)
         else:
             self.ids.rpm_digit_1.color = (1, 1, 1, 1)
         if self.current_speed < 100:
-            self.ids.rpm_digit_2.color = (0.5, 0.5, 0.5, 1)
+            self.ids.rpm_digit_2.color = (0.13, 0.13, 0.13, 1)
         else:
             self.ids.rpm_digit_2.color = (1, 1, 1, 1)
         if self.current_speed < 10:
-            self.ids.rpm_digit_3.color = (0.5, 0.5, 0.5, 1)
+            self.ids.rpm_digit_3.color = (0.13, 0.13, 0.13, 1)
         else:
             self.ids.rpm_digit_3.color = (1, 1, 1, 1)
 
@@ -101,8 +112,9 @@ class ServoApp(App):
 
     def update_rpm(self, root, dt):
         rpm = self.servo.get_rpm()
+        print(rpm)
         if rpm is not None:
-            root.current_speed = rpm
+            root.current_speed = abs(rpm)
             root.update_rpm_display()
 
 
